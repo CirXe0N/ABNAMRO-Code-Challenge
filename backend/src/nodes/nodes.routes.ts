d4de666377c8d3b1d6ff67ed4d34b35c4c2bced6
Dwayne <dwayne@cirxsoftware.com>
2021-01-08 11:16:26 +0100

    Add logic to the retrieve all nodes from nodes endpoint

--- backend/src/nodes/nodes.routes.ts
+++ backend/src/nodes/nodes.routes.ts
@@ -1,12 +1,18 @@
 import {Router as initRouter} from 'express';
 import {Router, Request, Response} from 'express-serve-static-core';
+import {Node} from './nodes.model';
 
 export class NodesRoutes {
     private router: Router = initRouter();
 
     public configure(): Router {
         this.router.get('/', (req: Request, res: Response) => {
-            res.status(200).send(`Hello World!`);
+           Node.retrieveAll()
+                .then((items) => {
+                    items.toJson().then((items) => {
+                        res.status(200).send(items);
+                    })
+                })
         });
 
         this.router.get('/:id', (req: Request, res: Response) => {
